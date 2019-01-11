36d7c991d0a8adc0e72b0f6dc337f4be0a58bdfd
stanislaw <stanislavbaraniuk@gmail.com>
2019-01-11 12:18:14 +0200

    serv

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-const express = require('express')
-const path = require('path')
-const history = require('connect-history-api-fallback')
-// ^ middleware to redirect all URLs to index.html
-
-const app = express()
-const staticFileMiddleware = express.static(path.join(__dirname))
-
-app.use(staticFileMiddleware)
-app.use(history())
-app.use(staticFileMiddleware)
-
-app.get('/', function (req, res) {
-    res.render(path.join(__dirname + '/index.html'))
-})
-
-app.listen(5000, function () {
-    console.log( 'Express serving on 5000!' )
-})
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
